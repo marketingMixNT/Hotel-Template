97fcdec5208e11d7418c183be8d5e454e78e7fe1
Marek <marekgacek45@gmail.com>
2023-11-24 14:23:26 +0100

    add cookieBar with PP cookie logic

--- src/js/main.js
+++ src/js/main.js
@@ -42,6 +42,41 @@ const setTodaysDate = () => {
     endDateLabel ?  endDateLabel.innerHTML = formattedFutureDate : ''
 };
 
+
+
+
+
+const cookieBox = document.querySelector("#cookieBox");
+const acceptBtn = document.querySelector("#acceptBtn");
+
+const addCookie = () => {
+    document.cookie = "Polityka Prywatności; max-age=" + 60 * 60 * 24 * 30;
+
+    if (document.cookie.indexOf("Polityka Prywatności") !== -1) {
+        cookieBox.classList.add("hidden");
+    }
+};
+
+const checkCookie = () => {
+    if (document.cookie.indexOf("Polityka Prywatności") !== -1) {
+        cookieBox.classList.add("hidden");
+    } else {
+        cookieBox.classList.remove("hidden");
+    }
+};
+
+checkCookie();
+
+acceptBtn.addEventListener("click", () => {
+    addCookie();
+    checkCookie();
+});
+
+
+
+
+
+
 setTodaysDate();
 
 footerYear ? footerYear.innerHTML = year : ''
